f32fbf8f3fd176015a4b49234ba630a9b2c786d9
Omer <omermatin22@gmail.com>
2025-05-06 14:13:05 -0400

    classes

---
+++ lib/models/class_schedule.dart
@@ -0,0 +1,15 @@
+class ClassSchedule {
+  final String courseName;
+  final String startTime;
+  final String endTime;
+  final String building;
+  final String mode;
+
+  ClassSchedule({
+    required this.courseName,
+    required this.startTime,
+    required this.endTime,
+    required this.building,
+    required this.mode
+  });
+}
---
+++ lib/models/garage.dart
@@ -0,0 +1,34 @@
+class Garage {
+  final String type;
+  final String name;
+  final int studentSpaces;
+  final int studentMaxSpaces;
+  final double availablePercent; 
+  final double latitude;
+  final double longitude;
+
+  Garage({
+    required this.type,
+    required this.name,
+    required this.studentSpaces,
+    required this.studentMaxSpaces,
+    required this.availablePercent,
+    required this.latitude,
+    required this.longitude,
+  });
+
+  factory Garage.fromJson(Map<String, dynamic> jsonData) {
+    final int current = int.tryParse(jsonData['studentSpaces'] ?? '0') ?? 0;
+    final int max = int.tryParse(jsonData['studentMaxSpaces'] ?? '1') ?? 1;
+
+    return Garage(
+      type: jsonData['type'] ?? '',
+      name: jsonData['name'] ?? '',
+      studentSpaces: current,
+      studentMaxSpaces: max,
+      availablePercent: (max > 0) ? (current / max * 100) : 0.0,
+      latitude: double.tryParse(jsonData['Latitude'] ?? '0') ?? 0.0,
+      longitude: double.tryParse(jsonData['Longitude'] ?? '0') ?? 0.0,
+    );
+  }
+}
